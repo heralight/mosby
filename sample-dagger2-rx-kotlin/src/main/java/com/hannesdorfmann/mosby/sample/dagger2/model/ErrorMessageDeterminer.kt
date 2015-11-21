303142e431184d2ef5a3cd5489371810d77043a5
heralight <heralight@users.noreply.github.com>
2015-11-21 17:59:33 +0100

    Add sample-dagger2-rx-kotlin

---
+++ sample-dagger2-rx-kotlin/src/main/java/com/hannesdorfmann/mosby/sample/dagger2/model/ErrorMessageDeterminer.kt
@@ -0,0 +1,5 @@
+package com.hannesdorfmann.mosby.sample.dagger2.model
+
+/**
+ * Created by Alexandre Richonnier on 21/11/15.
+ */
---
+++ sample-dagger2-rx-kotlin/src/main/java/com/hannesdorfmann/mosby/sample/dagger2/model/Repo.kt
@@ -0,0 +1,7 @@
+package com.hannesdorfmann.mosby.sample.dagger2.model
+
+/**
+ * Created by Alexandre Richonnier on 21/11/15.
+ */
+class Repo {
+}
